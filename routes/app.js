1390ddb4298018ebefe40803ca1351c0d5994e7d
erice04 <erlin879@gmail.com>
2021-03-18 23:11:43 -0400

    .

--- routes/app.js
+++ routes/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.set('view engine', 'ejs');
-const port = 80;
+const port = 800;
 
 app.use(express.static('public'));
 
 // Sends the webpage
 app.get("/", function (req, res) {
     console.log("User requested webpage");
-    res.render("index");
 });
 
 // Turns on the server
