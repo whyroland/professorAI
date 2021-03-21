53ee10d93443d38f88f9a03066222e0f4c1c6658
whyroland <42717361+whyroland@users.noreply.github.com>
2021-03-21 02:14:03 -0400

    wolfram minor changes

--- public/scripts/askprofconnect.js
+++ public/scripts/askprofconnect.js
@@ -14,19 +14,24 @@ form.onsubmit = function (event) {
 
     console.log(jsonData);
 
-    jQuery.ajax({
-        url: '/getAnswer',
-        dataType: "json",
-        data: JSON.stringify({Question: jsonData}),
-        cache: false,
-        contentType: "application/json; charset=utf-8",
-        processData: false,
-        method: 'POST',
-        type: 'POST',
-        success: function (data) {
-          console.log(data);
-          insertlocation.innerHTML = "<div class='card jello animated'><div class='card-header'><h5 class='mb-0' style='font-size: 'Inter';'>The Professor Says...</h5></div><div class='card-body'><p class='card-text' style='font-family: 'Inter';'><strong>" + data + "</strong></p></div></div>";
-        }
-    });
-    console.log("Data successfully sent to backend: question");
+    if (input.value.trim() != "") {
+        jQuery.ajax({
+            url: '/getAnswer',
+            dataType: "json",
+            data: JSON.stringify({Question: jsonData}),
+            cache: false,
+            contentType: "application/json; charset=utf-8",
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+            success: function (data) {
+              console.log(data);
+              var shake = " pulse animated";
+              if (data === "I do not know the answer to that question, maybe make sense next time" || data === "No short answer available")
+                shake = " jello animated";
+              insertlocation.innerHTML = "<div class='card" + shake + "'><div class='card-header'><h5 class='mb-0' style='font-size: 'Inter';'>The Professor Says...</h5></div><div class='card-body'><p class='card-text' style='font-family: 'Inter';'><strong>" + data + "</strong></p></div></div>";
+            }
+        });
+        console.log("Data successfully sent to backend: question");
+    }
 }
